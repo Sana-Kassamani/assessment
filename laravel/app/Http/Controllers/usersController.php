e00b07ecb63119c7e015c3cb025dab0c739e8deb
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 11:34:16 +0200

    Laravel <> Add - create user api

--- laravel/app/Http/Controllers/usersController.php
+++ laravel/app/Http/Controllers/usersController.php
@@ -13,5 +13,24 @@ class usersController extends Controller
             "users"=>$users
         ],200);
     }
+    public function create_user(Request $request){
+        if(!$request->name || !$request->color )
+        {
+            return response()->json([
+                "message"=>"All fields are required"
+            ],400);
+        }
+        $user = new User;
+        $user->name=$request->name;
+        $user->color = $request->color;
+        $user->save();
+        
+        return response()->json([
+            "message"=>"User created successfully",
+            "user"=>$user
+        ],201);
+    }
+
+
     
 }
