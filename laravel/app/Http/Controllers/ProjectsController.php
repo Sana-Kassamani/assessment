b5bdffc27fc8ceee26b6add7ee4529621d6fed4c
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 11:51:53 +0200

    Laravel <> Create - create  projects api

--- laravel/app/Http/Controllers/ProjectsController.php
+++ laravel/app/Http/Controllers/ProjectsController.php
@@ -13,5 +13,24 @@ class ProjectsController extends Controller
             "projects"=>$projects
         ],200);
     }
+    public function create_project(Request $request){
+        if(!$request->name || !$request->decription )
+        {
+            return response()->json([
+                "message"=>"All fields are required"
+            ],400);
+        }
+        $project = new project;
+        $project->name=$request->name;
+        $project->decription  = $request->decription ;
+        $project->save();
+        
+        return response()->json([
+            "message"=>"project created successfully",
+            "project"=>$project
+        ],201);
+
+    }
+
     
 }
