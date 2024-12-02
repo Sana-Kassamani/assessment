81761ff107340aedc18ceda86a12132541498ba9
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 11:35:58 +0200

    Laravel <> Capitalize - users controller class

--- laravel/app/Http/Controllers/usersController.php
+++ laravel/app/Http/Controllers/usersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class usersController extends Controller
+class UsersController extends Controller
 {
     public function get_users(){
         $users = User::all();
