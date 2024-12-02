525f6f9a43722f438f93d44f00c6867ccf4867cf
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 12:12:29 +0200

    React <> Create - project context

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
+import ProjectProvider from "./context/ProjectContext";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
+        <ProjectProvider>
+          <Routes>
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </ProjectProvider>
       </BrowserRouter>
     </div>
   );
---
+++ react-app/src/context/ProjectContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+import apiRoutes from "../utils/api";
+import axios from "axios";
+
+export const projectContext = createContext();
+
+const ProjectProvider = ({ children }) => {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    const apiUrl = apiRoutes.getProjects;
+    console.log(url);
+    const response = await axios.request({
+      method: "GET",
+      url: apiUrl,
+    });
+
+    setProjects(response.data.projects);
+  };
+  return (
+    <projectContext.Provider
+      value={{
+        projects,
+        getProjects,
+      }}
+    >
+      {children}
+    </projectContext.Provider>
+  );
+};
+
+export default ProjectProvider;
