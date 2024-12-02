cfdae7349688d4b29362bcdfb7beffd0e0114be2
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 11:57:06 +0200

    Laravel <> Create - request middleawre

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,11 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Middleware\RequestMiddleware;
 
-Route::prefix("/users")->group(function() {
-    Route::get("/",[UsersController::class, "get_users"]);
-    Route::post("/",[UsersController::class, "create_user"]);
-    Route::post("/{id}",[UsersController::class, "update_user"]);
-    Route::get("/{id}",[UsersController::class, "delete_user"]);
+Route::middleware(RequestMiddleware::class)->group(function(){
+    Route::prefix("/users")->group(function() {
+        Route::get("/",[UsersController::class, "get_users"]);
+        Route::post("/",[UsersController::class, "create_user"]);
+        Route::post("/{id}",[UsersController::class, "update_user"]);
+        Route::get("/{id}",[UsersController::class, "delete_user"]);
+    });
+    
+    Route::prefix("/projects")->group(function() {
+        Route::get("/",[UsersController::class, "get_projects"]);
+        Route::post("/",[UsersController::class, "create_project"]);
+        Route::post("/{id}",[UsersController::class, "update_project"]);
+        Route::get("/{id}",[UsersController::class, "delete_project"]);
+    });
 });
 
