4ac7b7dfa4e35da9d907d4f2149ff8b2adb1e8c4
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-02 11:45:56 +0200

    Laravel <> Add - delete user and update user routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -6,5 +6,8 @@ use App\Http\Controllers\UsersController;
 
 Route::prefix("/users")->group(function() {
     Route::get("/",[UsersController::class, "get_users"]);
+    Route::post("/",[UsersController::class, "create_user"]);
+    Route::post("/{id}",[UsersController::class, "update_user"]);
+    Route::get("/{id}",[UsersController::class, "delete_user"]);
 });
 
